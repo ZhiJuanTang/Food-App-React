89bf24d70c4f888332cecae64e4c1553043d3309
ZhiJuanTang <tangzhijuan56@gmail.com>
2022-09-03 19:32:47 +0200

    Managing Cart & Modal State

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Cart from "./componants/Cart/Cart";
 import Header from "./componants/Layout/Header";
 import Meals from "./componants/Meals/Meals";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  }
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  }
   return (
     <React.Fragment>
-      <Cart />
-      <Header />
+     {cartIsShown && <Cart onCloseCart ={hideCartHandler}/>}
+      <Header onShowCart ={showCartHandler} />
       <main>
         <Meals />
       </main>
--- src/componants/UI/Modal.js
+++ src/componants/UI/Modal.js
@@ -5,7 +5,7 @@ import ReactDom from "react-dom";
 //PORTAL example
 
 const Backdrop = (props) => {
-  return <div className={classes.backdrop} />;
+  return <div className={classes.backdrop} onClick={props.onClose} />;
 };
 const ModelOverlay = (props) => {
   return (
@@ -20,7 +20,11 @@ const portalElement = document.getElementById("overlays");
 const Modal = (props) => {
   return (
     <React.Fragment>
-      {ReactDom.createPortal(<Backdrop />, portalElement)}
+      {ReactDom.createPortal(
+        <Backdrop onClose={props.onCloseCart} />,
+        portalElement
+      )}
+      {/* <Backdrop> is children to <Modal> , so here onClose is passed from here <Modal> to <Backdrop> as a prop to 'onClick' . The Children parent relationship is more clear when <Backdrop> is written in a seperate componant */}
       {ReactDom.createPortal(
         <ModelOverlay>{props.children} </ModelOverlay>,
         portalElement
