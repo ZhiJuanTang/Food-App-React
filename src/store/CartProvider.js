f9da04bc8f1a9eb0d3dc3e501048ff9b7392ed99
ZhiJuanTang <tangzhijuan56@gmail.com>
2022-09-09 14:47:34 +0200

    first part(cart) is done

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,40 +1,53 @@
-import { useReducer } from 'react';
+import { useReducer } from "react";
+import CartContext from "./cart-context";
 
-import CartContext from './cart-context';
-
+//following 2 consts should be defined outside the default funtion CartProvider because they won't need anything any data inside of the funtion CartProvider. and they shouldn't be recreated every time when the CartProvider componant is revaluated
 const defaultCartState = {
   items: [],
   totalAmount: 0,
 };
 
 const cartReducer = (state, action) => {
-  if (action.type === 'ADD') {
+  // state : last state snapshot of the state managed by the reducer. action will update this state, 'return' a new state snapshot
+  //action will be dispatched by me later
+
+  // a state snapshot looks like this:
+  //   {items: [ item1: { id: props.id,
+  //        name: props.name,
+  //        amount: amount,
+  //        price: props.price,}, item2, item3....], 
+  //    totalAmount: }
+  if (action.type === "ADD") {
+    //update totalAmount:
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
 
+    //find the updated item
     const existingCartItemIndex = state.items.findIndex(
       (item) => item.id === action.item.id
     );
     const existingCartItem = state.items[existingCartItemIndex];
     let updatedItems;
 
+    // updatedItems & updatedItem:
     if (existingCartItem) {
       const updatedItem = {
         ...existingCartItem,
         amount: existingCartItem.amount + action.item.amount,
       };
-      updatedItems = [...state.items];
+      updatedItems = [...state.items]; //this CartItem exists already, so item list shouldn't be changed after 'ADD'
       updatedItems[existingCartItemIndex] = updatedItem;
     } else {
-      updatedItems = state.items.concat(action.item);
-    }
+      updatedItems = state.items.concat(action.item); //this CartItem doesn't exists yet, so this new item  should be added to item list
+    } // concat makes a new array. better than .push(same array. so push changes the react DOM without react knowing it).
 
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
-    };
+    }; // a new state snapshot is returned by reducer
   }
-  if (action.type === 'REMOVE') {
+  
+  if (action.type === "REMOVE") {
     const existingCartItemIndex = state.items.findIndex(
       (item) => item.id === action.id
     );
@@ -42,16 +55,16 @@ const cartReducer = (state, action) => {
     const updatedTotalAmount = state.totalAmount - existingItem.price;
     let updatedItems;
     if (existingItem.amount === 1) {
-      updatedItems = state.items.filter(item => item.id !== action.id);
+      updatedItems = state.items.filter((item) => item.id !== action.id);
     } else {
       const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
       updatedItems = [...state.items];
-      updatedItems[existingCartItemIndex] = updatedItem;
+      updatedItems[existingCartItemIndex] = updatedItem; // here means override this old item in items array with the updated item which had the updated amount
     }
 
     return {
       items: updatedItems,
-      totalAmount: updatedTotalAmount
+      totalAmount: updatedTotalAmount,
     };
   }
 
@@ -60,16 +73,18 @@ const cartReducer = (state, action) => {
 
 const CartProvider = (props) => {
   const [cartState, dispatchCartAction] = useReducer(
+    //useReducer always returns: [state snapshot, a funtion that dispatch an action which updates the snaphot]
     cartReducer,
     defaultCartState
   );
 
   const addItemToCartHandler = (item) => {
-    dispatchCartAction({ type: 'ADD', item: item });
+    dispatchCartAction({ type: "ADD", item: item });
+    //'type' is a identifier for reducer. 'item: item': forward item that we get to reducer
   };
 
   const removeItemFromCartHandler = (id) => {
-    dispatchCartAction({ type: 'REMOVE', id: id });
+    dispatchCartAction({ type: "REMOVE", id: id });
   };
 
   const cartContext = {
--- src/store/cart-context.js
+++ src/store/cart-context.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React from "react";
 
 const CartContext = React.createContext({
   items: [],
+  //each item details is defined in MealItem componant by calling 'addItem' funtion there, also updated by anywhere 'addItem' or 'removeItem' funtions are called
+  //   MealItem componant:
+  //   cartCtx.addItem({
+  //      id: props.id,
+  //      name: props.name,
+  //      amount: amount,
+  //      price: props.price,
+  // });
   totalAmount: 0,
   addItem: (item) => {},
-  removeItem: (id) => {}
+  removeItem: (id) => {},
 });
 
 export default CartContext;
